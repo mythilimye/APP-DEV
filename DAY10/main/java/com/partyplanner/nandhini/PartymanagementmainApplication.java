2a4b3f4ca3f6b252f5f6d892a8aff85402509fd7
MYTHILI S <119104953+mythilimye@users.noreply.github.com>
2024-03-27 09:52:47 +0530

    Add files via upload

---
+++ DAY10/main/java/com/partyplanner/nandhini/PartymanagementmainApplication.java
@@ -0,0 +1,13 @@
+package com.partyplanner.nandhini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartymanagementmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartymanagementmainApplication.class, args);
+	}
+
+}
